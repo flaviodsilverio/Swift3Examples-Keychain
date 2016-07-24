576dd9d1ff675aa2fc4d2a7678fc5f09c42d859d
flaviodsilverio <flaviosilverio@macbook-pro-de-flavio.local>
2016-07-24 10:15:41 +0100

    Initial Commit

---
+++ Swift3Keychain/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Swift3Keychain
+//
+//  Created by Flávio Silvério on 24/07/16.
+//  Copyright © 2016 Flávio Silvério. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
